4b54f5d05f7d84cc1441c2ace703f63ec5469f31
Chris G <howmuchiswhere@gmail.com>
2024-08-18 05:11:24 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# truncateline.nvim
+
+![eg](eg.png)
+
+### About
+
+A Neovim plugin for the prevention of getting lost when scrolling off into the yonder. When no longer visible, truncateline will display the first n characters of non-whitespace text of a line as virtual text. Now the text will appear to be truncated, which just adds another solution for dealing with very long lines, instead of having to rely on wrapping which is difficult to follow sometimes. This is very handy for editing database files and HTML, as well as pretty much anything a tiling window manager user might like to edit on a laptop. One way to use this would be to have it always on, and let the first 8 characters appear should you scroll to the right. This is the default, but it may be preferable to set the plugin to disabled, set a longer character count, and instead use the temporary toggle feature for a quick reminder, should you need it.
+
+### Features
++ Have the option to turn it on or off at start up.
++ As well as a toggle function, a key can also be bound to temporarily toggle between on and off.
++ Character count, truncation string, temporary toggle duration and hilight group can also be configured.
+
+### Install and configuration
+For Lazy, I suggest setting lazy loading to false. Just in case you like to bind your keys with Lazy, which will prevent the plugin from starting until they are used.
+```
+    { "rlychrisg/truncateline.nvim", lazy = false },
+```
+The plugin will also need to be called somewhere with `require('truncateline').setup()`.
+
+For a default configuration:
+```
+    {
+        "rlychrisg/truncateline.nvim",
+        lazy = false,
+        config = function ()
+            require('truncateline').setup({
+
+                enabled_on_start = true,
+
+                -- the truncate_str will be appended to virtual text to help
+                -- distinguish it from the actual text. to remove it entirely
+                -- just delete the contents of the quotes, leaving "".
+                truncate_str = "...",
+
+                -- how many characters from the start of the line should be
+                -- displayed. note: you might want the total of this setting,
+                -- along with truncate_str, to be less than your sidescrolloff
+                -- setting, to prevent obscuring text.
+                line_start_length = 8,
+
+                -- for the temporary toggle function, this sets the time in ms
+                -- before returning to on/off.
+                temporary_toggle_dur = 2000,
+
+                -- which hilight group should be used for the virtual text.
+                -- "Comment", or "Normal" are good choices, but anything in
+                -- :highlight can be used.
+                hilight_group = "Comment"
+            })
+        end
+    }
+```
+
+### Setting keybinds
+No default keybinds are set because I like to avoid breaking other plugins. This means you'll have to come up with your own though. Here are my suggestions, and feel free to use both, either, or neither.
+```
+    vim.api.nvim_set_keymap('n', '<leader>l', ':lua require("truncateline").TemporaryToggle()<CR>',
+    {  noremap = true, silent = true, desc = "TruncateLine temporary toggle" })
+    vim.api.nvim_set_keymap('n', '<leader>sl', ':lua require("truncateline").ToggleTruncate()<CR>',
+    { noremap = true, silent = true, desc = "TruncateLine toggle" })
+```
+
+### A note about contributions and suggestions
+Both are welcome but I'm a bit of a beginner in both lua and git, so please be patient and err on the side of patronising when explaining stuff.
+
+
---
+++ lua/truncateline/init.lua
@@ -0,0 +1,110 @@
+local M =  {}
+
+M.config = {
+    enabled_on_start = true,
+    truncate_str = "...",
+    line_start_length = 8,
+    temporary_toggle_dur = 2000,
+    hilight_group = "Comment"
+}
+
+-- some vars for readability
+local current_buffer = vim.api.nvim_get_current_buf()
+local augroup = vim.api.nvim_create_augroup('VirtualText', { clear = true })
+local virt_text_ns = vim.api.nvim_create_namespace('virtual_text_namespace')
+
+function M.setup(opts)
+    -- if different values are passed to user config, use those instead
+    M.config = vim.tbl_deep_extend("force", M.config, opts or {})
+
+    -- Function to display virtual text
+    local function truncate_line()
+
+        -- clear existing virtual text
+        vim.api.nvim_buf_clear_namespace(current_buffer, virt_text_ns, 0, -1)
+
+        -- get first and last lines visible on screen
+        local start_line = vim.fn.line('w0')
+        local end_line = vim.fn.line('w$')
+
+        for line = start_line - 1, end_line - 1 do
+            -- store line content in var
+            local line_content = vim.api.nvim_buf_get_lines(current_buffer, line, line + 1, false)[1]
+
+            -- if line content isn't nil or empty (will still show truncate char if line has spaces)
+            if line_content and line_content ~= "" then
+
+                local function hidden_start(line_number)
+                    -- compare first non white space char with window view
+                    -- and return true if it is hidden
+                    local view = vim.fn.winsaveview()
+                    local first_char_col = vim.fn.indent(line_number)
+                    return first_char_col < view.leftcol
+                end
+
+                local line_number = line + 1 -- i have no idea why i need to adjust for index here
+
+                if hidden_start(line_number) then
+                    -- Get the first n non whitespace characters, handle empty or whitespace-only lines
+                    local virtual_text = line_content:match("^%s*(%S.-)%s*$") or ""
+                    -- trim the string to the given char count
+                    local line_start_length = M.config.line_start_length
+                    virtual_text = virtual_text:sub(1, line_start_length)
+                    -- adds the truncate characters, if given
+                    local truncate_str = M.config.truncate_str
+                    virtual_text = virtual_text .. truncate_str
+
+                    -- Set the virtual text on the left side
+                    local hilight_group = M.config.hilight_group
+                    vim.api.nvim_buf_set_extmark(current_buffer, virt_text_ns, line, 0, {
+                        virt_text = { { virtual_text, hilight_group } },
+                        hl_mode = "replace",
+                        virt_text_pos = "overlay",
+                    })
+                end
+            end
+        end
+    end
+
+    -- if enabled, trigger autocmd
+    local function create_ac()
+        if _G.is_truncate_enabled then
+            vim.api.nvim_create_autocmd({ "CursorMoved", "WinScrolled" }, {
+                group = augroup,
+                callback = truncate_line,
+            })
+        end
+    end
+
+    function M.ToggleTruncate()
+        if _G.is_truncate_enabled then
+            _G.is_truncate_enabled = false
+            print('TruncateLine disabled')
+            vim.api.nvim_buf_clear_namespace(current_buffer, virt_text_ns, 0, -1)
+            vim.cmd[[autocmd! VirtualText]]
+        else
+            _G.is_truncate_enabled = true
+            create_ac()
+            truncate_line()
+            print('TruncateLine enabled')
+        end
+    end
+
+    -- quickly turn on/off
+    function M.TemporaryToggle()
+        M.ToggleTruncate()
+        local temporary_toggle_dur = M.config.temporary_toggle_dur
+        -- timer to return to previous on/off state
+        vim.defer_fn(M.ToggleTruncate, temporary_toggle_dur)
+    end
+
+    -- enable on start?
+    if M.config.enabled_on_start then
+        _G.is_truncate_enabled = true
+        create_ac()
+    end
+
+end
+
+return M
+
